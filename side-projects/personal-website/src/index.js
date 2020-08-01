d162a35dfb7396bbc10beda149b0229f1d067a0e
Brandon Artigue <bartigue87@yahoo.com>
2020-07-31 18:49:22 -0600

    personal website

---
+++ side-projects/personal-website/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./basic.css";
+import App from "./App";
+
+ReactDOM.render(<App name="Brandon" />, document.getElementById("root"));
