838cefd444c07679c2f38c5a6a76734ee000847c
Brandon Artigue <bartigue87@yahoo.com>
2020-09-01 20:42:24 -0600

    beer app start

---
+++ side-projects/beer-api/app.js
@@ -0,0 +1,41 @@
+// variables
+const urlBase = "https://api.punkapi.com/v2/beers";
+
+// Create an async function called "getBeers" that uses fetch to get our beer data from the urlBase.
+// Render each beer name inside the div with the class of beers that currently exists in the HTML file.
+
+async function getBeers() {
+  const beerGetter = await fetch(urlBase);
+  const beerJson = await beerGetter.json();
+
+  const beersDiv = document.querySelector(".beers");
+
+  let beerHtml = "";
+
+  beerJson.forEach((beer) => {
+    beerHtml += `
+      <div class="beer-wrapper card">
+        <div class='beer'>
+            <img class='beer__img' src="${beer.image_url}">
+            <h3>${beer.name}</h3>   
+            <span class='beer__info'>
+                <span>ABV: ${beer.abv}%</span>
+                <span>IBU: ${beer.ibu}</span>
+            </span> 
+        </div>
+        <div class='beer__content'>
+                <div class='beer__name'>${beer.name}</div>
+                <div class='beer__tagline'>${beer.tagline}</div>
+                <div class='beer__description'>${beer.description}</div>
+                <div class='beer__food-pairing'>
+                    Pair with: ${beer.food_pairing.join(", ")} 
+                </div>
+            </div>
+      </div>
+      
+      `;
+  });
+
+  beersDiv.innerHTML = beerHtml;
+}
+getBeers();
